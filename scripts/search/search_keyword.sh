f0f746b8609f81017288f743341a2fbb687c8c6f
Andy Pearson <andy@londonparkour.com>
2024-06-10 11:44:43 +0100

    search keyword nad url

--- scripts/search/search_keyword.sh
+++ scripts/search/search_keyword.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-# Check if a QUERY is provided
+# Check if a keyword is provided
 if [ -z "$1" ]; then
-  echo "Usage: $0 <QUERY>"
+  echo "Usage: $0 <keyword>"
   exit 1
 fi
 
-# Store the QUERY
-QUERY="$1"
+# Store the keyword
+KEYWORD="$1"
 LIMIT="${2:-3}"
 
 # Temporary file to store search results
 TEMP_FILE="raw.json"
 URL_FILE="results.json"
 
-# Search YouTube for the QUERY and get the details of the 10 newest results
-yt-dlp --get-id --playlist-end $LIMIT "$QUERY"  > "$TEMP_FILE"
+# Search YouTube for the keyword and get the details of the 10 newest results
+yt-dlp --flat-playlist -j "ytsearchdate$LIMIT:$KEYWORD" > "$TEMP_FILE"
 
 # Prepare the JSON structure
 echo '{"results":[' > "$URL_FILE"
@@ -24,12 +24,17 @@ echo '{"results":[' > "$URL_FILE"
 FIRST=1
 while IFS= read -r line; do
 
-  VIDEO_URL="https://www.youtube.com/watch?v=$line"
+  VIDEO_ID=$(echo "$line" | jq -r '.id')
+  VIDEO_URL="https://www.youtube.com/watch?v=$VIDEO_ID"
 
   if [ $FIRST -eq 0 ]; then
     echo ',' >> "$URL_FILE"
   fi
 
+  # Adds the Download URL if needed.
+  # DOWNLOAD_URL=$(yt-dlp -f b --get-url "$VIDEO_URL")
+  # echo '{"video":"'"$VIDEO_URL"'","download":"'"$DOWNLOAD_URL"'"}' >> "$URL_FILE"
+  
   echo '{"video":"'"$VIDEO_URL"'"}' >> "$URL_FILE"
   FIRST=0
 done < "$TEMP_FILE"
