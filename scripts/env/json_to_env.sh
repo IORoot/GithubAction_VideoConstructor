04aee253c2dd7f298cb79531cf3c8a33b0f28134
Andy Pearson <andy@londonparkour.com>
2024-06-15 17:10:49 +0100

    checks

---
+++ scripts/env/json_to_env.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Read and flatten JSON into environment variables
+jq -r '
+def to_env: gsub("[^a-zA-Z0-9]"; "_") | ascii_upcase;
+
+# Recursive function to flatten nested objects and output as environment variables
+def recurse_to_env($prefix; $obj):
+  ($obj | to_entries) | .[] | (
+    if (.value | type) == "object" then
+      recurse_to_env("\($prefix)_\(.key | to_env)"; .value)
+    else
+      "\($prefix)_\(.key | to_env)=\"\(.value|tostring)\""
+    end
+  );
+
+# Start recursion from the root object
+recurse_to_env("VC"; .)
+' config.json | while IFS= read -r line; do
+  echo "$line"
+  # echo "$line" >> $GITHUB_ENV
+done
