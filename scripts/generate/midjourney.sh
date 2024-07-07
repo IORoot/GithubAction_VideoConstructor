f625f14c929897eca3ab5d4e907d0804fabc386f
Andy Pearson <andy@londonparkour.com>
2024-07-07 11:16:31 +0100

    Midjourney to workflow

---
+++ scripts/generate/midjourney.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+if [[ "${DEBUG-0}" == "1" ]]; then set -o xtrace; fi        # DEBUG=1 will show debugging.
+
+# ╭──────────────────────────────────────────────────────────╮
+# │                        VARIABLES                         │
+# ╰──────────────────────────────────────────────────────────╯
+FOLDER="./scripts/generate"
+PWD=$(pwd)
+
+
+# ╭──────────────────────────────────────────────────────────╮
+# │                          Usage.                          │
+# ╰──────────────────────────────────────────────────────────╯
+
+usage()
+{
+    if [ "$#" -lt 1 ]; then
+        printf "ℹ️ Usage:\n $0 --json [data.json] \n\n" >&2 
+
+        printf "Summary:\n"
+        printf "This will switch the output to the correct script.\n\n"
+
+        printf "Flags:\n"
+
+        printf " --json <FILE>\n"
+        printf "\tThe JSON file to read.\n\n"
+
+        exit 1
+    fi
+}
+
+
+# ╭──────────────────────────────────────────────────────────╮
+# │         Take the arguments from the command line         │
+# ╰──────────────────────────────────────────────────────────╯
+function arguments()
+{
+    POSITIONAL_ARGS=()
+
+    while [[ $# -gt 0 ]]; do
+    case $1 in
+
+
+        --json)
+            JSON="$2"
+            shift
+            shift
+            ;;
+
+
+        -*|--*)
+            echo "Unknown option $1"
+            exit 1
+            ;;
+
+
+        *)
+            POSITIONAL_ARGS+=("$1") # save positional arg back onto variable
+            shift                   # remove argument and shift past it.
+            ;;
+    esac
+    done
+
+}
+
+
+# ╭──────────────────────────────────────────────────────────╮
+# │     Run these checks before you run the main script      │
+# ╰──────────────────────────────────────────────────────────╯
+function pre_flight_checks()
+{
+    
+    if [ ! -f "$JSON" ]; then
+        printf "No file found."
+    fi
+
+}
+
+
+# ╭──────────────────────────────────────────────────────────╮
+# │                                                          │
+# │                      Main Function                       │
+# │                                                          │
+# ╰──────────────────────────────────────────────────────────╯
+function main()
+{
+
+    pre_flight_checks
+
+    JSON_CONTENT=$(cat "$JSON")
+    JSON_KEYS=$(echo "$JSON_CONTENT" | jq -r 'keys[]')
+
+    # Iterate through each top-level key in the JSON
+    for section in $JSON_KEYS; do
+
+        # Remove digits from the end of the section name to get the script name
+        base_section_name=$(echo "$section" | grep -o '^[a-zA-Z_-]*')
+        # script_name="output_${base_section_name}.sh"
+
+        # Extract the run value
+        run=$(echo "$JSON_CONTENT" | jq -r --arg section "$section" '.[$section].run')
+        prompt=$(echo "$JSON_CONTENT" | jq -r --arg section "$section" '.[$section].prompt')
+
+        # Proceed only if run is true
+        if [ "$run" == true ]; then
+            npx tsx imagine.ts ${prompt}
+        fi
+        
+    done
+
+}
+
+usage "$@"
+arguments "$@"
+main "$@"
