5e405a4236c45c8994df43a40019e600c3686b07
Andy Pearson <andy@londonparkour.com>
2024-07-08 09:06:11 +0100

    Update midjourney runner

--- scripts/generate/midjourney.sh
+++ scripts/generate/midjourney.sh
@@ -76,17 +76,11 @@ function pre_flight_checks()
 
 }
 
-
-# ╭──────────────────────────────────────────────────────────╮
-# │                                                          │
-# │                      Main Function                       │
-# │                                                          │
-# ╰──────────────────────────────────────────────────────────╯
-function main()
+# ╭───────────────────────────────────────────────────────╮
+# │     Loop through JSON and run midjourney on each      │
+# ╰───────────────────────────────────────────────────────╯
+function run_midjourney()
 {
-
-    pre_flight_checks
-
     JSON_CONTENT=$(cat "$JSON")
     JSON_KEYS=$(echo "$JSON_CONTENT" | jq -r 'keys[]')
 
@@ -109,6 +103,69 @@ function main()
         fi
         
     done
+}
+
+
+# ╭───────────────────────────────────────────────────────╮
+# │     Download all images from the output TXT files      │
+# ╰───────────────────────────────────────────────────────╯
+function download_images()
+{
+    # Input file containing URLs
+    INPUT_FILE="$1"
+
+    # Check if input file exists
+    if [ ! -f "$INPUT_FILE" ]; then
+        echo "File not found: $INPUT_FILE"
+        exit 0
+    fi
+
+    # Get name of TXT file and remove extension
+    FILENAME_NO_EXTENSION="${INPUT_FILE%.*}"
+
+    if [[ $FILENAME_NO_EXTENSION == *"quad"* ]]; then
+        TYPE="quad"
+    elif [[ $FILENAME_NO_EXTENSION == *"upscaled"* ]]; then
+        TYPE="upscaled"
+    else
+        TYPE="none"
+    fi
+
+    # Read each line (URL) from the input file
+    while IFS= read -r URL; do
+
+        # Extract the part of the URL after the last slash and before the first question mark
+        FILENAME=$(basename "$URL" | awk -F '?' '{print $1}')
+        
+        # Remove the suffix to get the descriptive part
+        DESCRIPTIVE_PART=$(echo "$FILENAME" | sed -E 's/[_-][[:alnum:]]{8}-[[:alnum:]]{4}-[[:alnum:]]{4}-[[:alnum:]]{4}-[[:alnum:]]{12}\.png//')
+
+        # Get new timestamp
+        TIMESTAMP=$(date +%s)
+
+        # Format the output filename with sequential suffix
+        OUTPUT_FILE=$(printf "mj_%s_%s_%s.png" "$TYPE" "$DESCRIPTIVE_PART" "$TIMESTAMP")
+        
+        # Download the image using curl
+        curl -# -o "$OUTPUT_FILE" "$URL"
+
+    done < "$INPUT_FILE"
+}
+
+# ╭──────────────────────────────────────────────────────────╮
+# │                                                          │
+# │                      Main Function                       │
+# │                                                          │
+# ╰──────────────────────────────────────────────────────────╯
+function main()
+{
+
+    pre_flight_checks
+
+    run_midjourney
+
+    download_images "midjourney_images_quad_urls.txt"
+    download_images "midjourney_images_upscaled_urls.txt"
 
 }
 
