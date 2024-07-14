cb9b7e1edbd9c1571d2922a31ae3a40e64f80418
Andy Pearson <andy@londonparkour.com>
2024-07-14 11:59:05 +0100

    update env script to handle newlines

--- scripts/env/json_to_env.sh
+++ scripts/env/json_to_env.sh
@@ -1,36 +1,8 @@
 #!/bin/bash
 
-
-
-# # Read and flatten JSON into environment variables
-# jq -r '
-# def to_env: gsub("[^a-zA-Z0-9]"; "_") | ascii_upcase;
-
-# # Recursive function to flatten nested objects and output as environment variables
-# def recurse_to_env($prefix; $obj):
-#   ($obj | to_entries) | .[] | (
-#     if (.value | type) == "object" then
-#       recurse_to_env("\($prefix)_\(.key | to_env)"; .value)
-#     else
-#       "\($prefix)_\(.key | to_env)=\(.value|tostring)"
-#     end
-#   );
-
-# # Start recursion from the root object
-# recurse_to_env("VC"; .)
-# ' config.json | while IFS= read -r line; do
-#   echo "$line"
-#   echo "$line" >> $GITHUB_ENV
-# done
-
-
 # Read and flatten JSON into environment variables
 jq -r '
 def to_env: gsub("[^a-zA-Z0-9]"; "_") | ascii_upcase;
-
-# Function to escape newlines
-def escape_newlines:
-  gsub("\r\n"; "__NEWLINE__") | gsub("\n"; "__NEWLINE__") | gsub("\r"; "__NEWLINE__");
 
 # Recursive function to flatten nested objects and output as environment variables
 def recurse_to_env($prefix; $obj):
@@ -38,7 +10,7 @@ def recurse_to_env($prefix; $obj):
     if (.value | type) == "object" then
       recurse_to_env("\($prefix)_\(.key | to_env)"; .value)
     else
-      "\($prefix)_\(.key | to_env)=\(.value | tostring | escape_newlines)"
+      "\($prefix)_\(.key | to_env)=\(.value|tostring)"
     end
   );
 
